736bfb1e2ec0edd5c19ce656b72abafb9c176134
urban1999 <157912552+urban1999@users.noreply.github.com>
2024-03-10 16:55:20 -0400

    Add files via upload

---
+++ main-2.py
@@ -0,0 +1,20 @@
+def calculator():
+    a = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    b = float(input("Enter second number: "))
+    
+def sum(a, b):
+    answer = a + b
+    print("Answer is: " + str(answer))
+
+def diff(a, b):
+    answer = a - b
+    print("Answer is: " + str(answer))
+
+def mult(a, b):
+    answer = a * b
+    print("Answer is: " + str(answer))
+
+def div(a, b):
+    answer = a / b
+    print("Answer is: " + str(answer))
