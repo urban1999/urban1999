2c09dc0af6acefa7658eb937e5e2240b1b9d38ec
urban1999 <157912552+urban1999@users.noreply.github.com>
2024-03-10 17:21:48 -0400

    Update and rename main-2.py to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
-def calculator():
+def main():
     a = float(input("Enter first number: "))
     operator = input("Enter operator (+, -, *, /): ")
     b = float(input("Enter second number: "))
+
+    if operator == "+":
+        sum(a, b)
+    elif operator == "-":
+        diff(a, b)
+    elif operator == "*":
+        mult(a, b)
+    elif operator == "/":
+        div(a, b)
+    else:
+        print("Error! Please try again.")
+        main()
+
+    restart = input("Would you like to try another calculation? Enter Y or N.")
+    
+    if restart == "Y":
+        main()
     
 def sum(a, b):
     answer = a + b
@@ -18,3 +35,5 @@ def mult(a, b):
 def div(a, b):
     answer = a / b
     print("Answer is: " + str(answer))
+
+main()
